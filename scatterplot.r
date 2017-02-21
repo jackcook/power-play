66e537332f3d3f93f5b33128ecd533faacef7951
Jack Cook <hello@jackcook.nyc>
2017-02-21 19:18:03 +0100

    Remove axes and fit image correctly

--- scatterplot.r
+++ scatterplot.r
@@ -21,8 +21,15 @@ ggplot(dat, aes(x = x, y = y)) +
   annotation_custom(g, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
   theme(panel.background = element_rect(fill = "#2e9726"),
         panel.grid.major = element_line(color = "#2e9726"),
-        panel.grid.minor = element_line(color = "#2e9726")) +
-  coord_fixed(ratio = 2.3764) +
-  xlim(0, 1) +
-  ylim(-1, 0) +
-  geom_point(shape = 1)
+        panel.grid.minor = element_line(color = "#2e9726"),
+        plot.margin = unit(c(1, 1, 0.9, 1), "cm"),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank()) +
+  coord_fixed(ratio = 2.1739) +
+  scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(-1, 0), expand = c(0, 0)) +
+  geom_point(color = "#444444", size = 1)
+
+grid.text(player_one, y = unit(0.95, "npc"))
+grid.text(player_two, y = unit(0.05, "npc"))
