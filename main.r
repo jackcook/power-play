85b59855b0f901cf7cf35d6d8df0f0076f40bd9c
Jack Cook <hello@jackcook.nyc>
2017-02-21 22:20:47 +0100

    Moved court scatterplot into a function and displayed graph with Rmarkdown

--- main.r
+++ main.r
@@ -1,35 +1,7 @@
+setwd("/Users/v-jacco/Desktop/power-play")
+
 require(XML)
+source("scatterplot.r")
 
 data <- xmlParse("~/Desktop/power-play/in.game")
-names <- xpathSApply(data, "//string", xmlValue)
-player_one <- names[1]
-player_two <- names[2]
-
-coordinates <- xpathSApply(data, "//real", xmlValue)[-c(1, 2)]
-x <- coordinates[seq(1, length(coordinates), 2)]
-y <- coordinates[seq(1, length(coordinates), 2) + 1]
-
-dat <- matrix(rep(0, 121), ncol=11)
-
-for (i in seq(length(x))) {
-  x_index <- round(as.numeric(x[i]) / 0.1) + 1
-  y_index <- round(as.numeric(y[i]) / 0.1) + 1
-  dat[x_index,y_index] <- dat[x_index,y_index] + 1
-}
-
-dat2 <- dat %>%
-  tbl_df() %>%
-  rownames_to_column('X') %>%
-  gather(Y, value, -X) %>%
-  mutate(
-    X = factor(X, levels=1:11),
-    Y = factor(gsub("V", "", Y), levels=11:1)
-  )
-
-tile <- aes(fill = value)
-label <- aes(label = ifelse(value > 0, as.character(value), ""))
-
-ggplot(dat2, aes(X, Y)) +
-  geom_tile(tile) +
-  geom_text(label) +
-  scale_fill_gradient(low = "white", high = "steelblue")
+generate_court_scatterplot(data)
--- scatterplot.r
+++ scatterplot.r
@@ -1,37 +1,34 @@
 library(grid)
 library(png)
 library(tidyverse)
+library(XML)
 
-require(XML)
+generate_court_scatterplot <- function(data) {
+  coordinates <- as.numeric(xpathSApply(data, "//real", xmlValue)[-c(1, 2)])
+  x <- coordinates[seq(1, length(coordinates), 2)]
+  y <- coordinates[seq(1, length(coordinates), 2) + 1] * -1
+  dat <- data.frame(x = x, y = y)
 
-data <- xmlParse("~/Desktop/power-play/in.game")
-names <- xpathSApply(data, "//string", xmlValue)
-player_one <- names[1]
-player_two <- names[2]
+  img <- readPNG("~/Desktop/power-play/court.png")
+  g <- rasterGrob(img, interpolate = TRUE)
 
-coordinates <- as.numeric(xpathSApply(data, "//real", xmlValue)[-c(1, 2)])
-x <- coordinates[seq(1, length(coordinates), 2)]
-y <- coordinates[seq(1, length(coordinates), 2) + 1] * -1
-dat <- data.frame(x = x, y = y)
+  court_plot <- ggplot(dat, aes(x = x, y = y)) +
+    annotation_custom(g, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+    theme(panel.background = element_rect(fill = "#2e9726"),
+          panel.grid.major = element_line(color = "#2e9726"),
+          panel.grid.minor = element_line(color = "#2e9726"),
+          plot.margin = unit(c(0, 0, 0, 0), "mm"),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title = element_blank()) +
+    coord_fixed(ratio = 2.1739) +
+    scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(-1, 0), expand = c(0, 0)) +
+    geom_point(color = "#444444", size = 1)
 
-img <- readPNG("~/Desktop/power-play/court.png")
-g <- rasterGrob(img, interpolate = TRUE)
-
-ggplot(dat, aes(x = x, y = y)) +
-  annotation_custom(g, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
-  theme(panel.background = element_rect(fill = "#2e9726"),
-        panel.grid.major = element_line(color = "#2e9726"),
-        panel.grid.minor = element_line(color = "#2e9726"),
-        plot.margin = grid::unit(c(0, 0, 0, 0), "mm"),
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank()) +
-  coord_fixed(ratio = 2.1739) +
-  scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
-  scale_y_continuous(limits = c(-1, 0), expand = c(0, 0)) +
-  geom_point(color = "#444444", size = 1)
-
-ggsave("~/Desktop/power-play/export.png",
-       width = 7.01,
-       height = 15.24,
-       units = "cm")
+  ggsave("~/Desktop/power-play/export.png",
+         plot = court_plot,
+         width = 7.01,
+         height = 15.24,
+         units = "cm")
+}
